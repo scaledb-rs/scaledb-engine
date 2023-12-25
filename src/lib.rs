22d8f4257336548c6d00272f23df904d5774a0e4
kit-scaledb <154653603+kit-scaledb@users.noreply.github.com>
2023-12-24 23:47:28 -0400

    Initial Cargo project

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+// Copyright (c) ScaleDB LLC; used with permission
+// Licensed under the MIT License
+
+mod scaledb_engine;
+pub use scaledb_engine::ScaledbEngine;
---
+++ src/scaledb_engine.rs
@@ -0,0 +1,5 @@
+// Copyright (c) ScaleDB LLC; used with permission
+// Licensed under the MIT License
+
+pub struct ScaledbEngine {
+}
